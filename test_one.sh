2e8831970376395546d65ed434b75dc341414ff8
Karol Wrobel <eepomoc@gmail.com>
2012-05-31 00:54:24 +0200

    Kosmetyka.

--- test_one.sh
+++ test_one.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-my_name_is="uname -a"
-my_name_is+=" HELLO WORLD!"
+# HELLO :)
+
+my_name_is=`uname -n`
+my_name_is+=" says "
+my_name_is+="HELLO WORLD! "
+my_name_is+="to "
+my_name_is+=`uname -s`
+my_name_is+=" community."
 echo "${my_name_is}"
