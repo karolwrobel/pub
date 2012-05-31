104c9bfc0cbd08f8b0c32ee7b37e9dd14f701e23
Karol Wrobel <eepomoc@gmail.com>
2012-05-31 22:08:38 +0200

    Update readme + 2 male toole

---
+++ minitoole/pogoda.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+y=`date +%Y`
+m=`date +%m`
+d=`date +%d`
+h=`date +%H`
+
+if [ $h -lt 6 ] && [ $d -gt 1 ]; then
+let d--
+fi
+
+sdate=${y}${m}${d}
+
+for i in 18 12 06 00
+do
+
+	my_date=${sdate}${i}
+	adres="http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=${my_date}&row=406&col=233&lang=pl"
+
+	wget $adres -O /home/ok/Desktop/pogoda_${my_date}.png
+
+	if [ `ls -al /home/ok/Desktop/pogoda_${my_date}.png | cut -f5 -d' '` -gt 10000 ]
+	then
+		exit
+	else
+		rm -f /home/ok/Desktop/pogoda_${my_date}.png
+	fi
+done
+
